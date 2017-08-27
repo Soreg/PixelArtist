4b59ad7e990f0f7b83551e334f2c0edc8bbd6c9a
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-08-27 23:31:11 +0200

    Functionality: Added sizes (Large, Medium, Small) to project

--- js/app.js
+++ js/app.js
@@ -5,25 +5,61 @@ var pixels = document.getElementsByClassName("pixel");
 var remove = document.querySelector("#remove");
 //console.log(selects.length);
 //alert(window.getComputedStyle(document.getElementById('colorPosition2'),null).getPropertyValue('background'));
-
-for (var i = 0; i < colors.length; i++) {
-  colors[i].addEventListener('click', function() {
-    colorPicked = window.getComputedStyle(this, null).getPropertyValue("background-color");
-  });
-}
-
-for (var i = 0; i < pixels.length; i++) {
-  pixels[i].addEventListener('mousedown', function() {
-    this.style.backgroundColor = colorPicked;
-  });
-}
-
-remove.addEventListener('click', function() {
-  for (var i = 0; i < pixels.length; i++) {
-    pixels[i].style.backgroundColor = "#FFF";
+function draw() {
+  function changeSize() {
+    var val = document.getElementById("pixelSize").value;
+    drawPixels(val);
   }
-})
-picker.addEventListener("change", watchColorPicker, false);
-function watchColorPicker(event) {
-  colorPicked = picker.value;
+
+  function drawPixels(val) {
+    var element = document.querySelector(".pixelContainer");
+    element.innerHTML = "";
+    var pixel;
+    if (val === "large") {
+      for (var i = 0; i < 100; i++) {
+        pixel = document.createElement("div");
+        pixel.className = "pixel pixelLarge";
+        element.appendChild(pixel);
+      }
+    } else if (val === "medium") {
+      for (var i = 0; i < 400; i++) {
+        pixel = document.createElement("div");
+        pixel.className = "pixel pixelMedium";
+        element.appendChild(pixel);
+      }
+    } else {
+      for (var i = 0; i < 1600; i++) {
+        pixel = document.createElement("div");
+        pixel.className = "pixel pixelSmall";
+        element.appendChild(pixel);
+      }
+    }
+  }
+
+  changeSize();
+
+  for (var i = 0; i < colors.length; i++) {
+    colors[i].addEventListener('click', function() {
+      colorPicked = window.getComputedStyle(this, null).getPropertyValue("background-color");
+    });
+  }
+
+  for (var i = 0; i < pixels.length; i++) {
+    pixels[i].addEventListener('mousedown', function() {
+      this.style.backgroundColor = colorPicked;
+    });
+  }
+
+  remove.addEventListener('click', function() {
+    for (var i = 0; i < pixels.length; i++) {
+      pixels[i].style.backgroundColor = "#FFF";
+    }
+  })
+
+  picker.addEventListener("change", watchColorPicker, false);
+  function watchColorPicker(event) {
+    colorPicked = picker.value;
+  }
 }
+
+draw();
