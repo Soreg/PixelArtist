de855011ef839e0ec62b1008f793164630544f19
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-08-28 02:51:19 +0200

    Added comments

--- js/app.js
+++ js/app.js
@@ -3,15 +3,22 @@ var colors = document.getElementsByClassName("userColor");
 var picker = document.getElementById("colorPicker");
 var pixels = document.getElementsByClassName("pixel");
 var remove = document.querySelector("#remove");
+var addColor = document.getElementById("addColorBtn");
+var customColors = document.getElementById("customs");
 var drawing = false;
-//console.log(selects.length);
-//alert(window.getComputedStyle(document.getElementById('colorPosition2'),null).getPropertyValue('background'));
+// Initial function
 function draw() {
+  // Run function to change color
+  for (var i = 0; i < colors.length; i++) {
+    colors[i].addEventListener("click", selectedColor())
+  }
+  // Change size (number of pixels)
   function changeSize() {
     var val = document.getElementById("pixelSize").value;
     drawPixels(val);
   }
 
+  // Draws pixels based on size
   function drawPixels(val) {
     var element = document.querySelector(".pixelContainer");
     element.innerHTML = "";
@@ -36,44 +43,68 @@ function draw() {
       }
     }
   }
+
   changeSize();
-  for (var i = 0; i < colors.length; i++) {
-    colors[i].addEventListener('click', function() {
-      colorPicked = window.getComputedStyle(this, null).getPropertyValue("background-color");
-    });
+
+  // Changes color to colorpicker
+  picker.addEventListener("change", watchColorPicker, false);
+  function watchColorPicker(event) {
+    colorPicked = picker.value;
   }
+  // Changes color of pixels
   for (var i = 0; i < pixels.length; i++) {
     pixels[i].addEventListener('mousedown', function() {
       this.style.backgroundColor = colorPicked;
     });
   }
 
+ // Removes / resets canvas
   remove.addEventListener('click', function() {
     for (var i = 0; i < pixels.length; i++) {
       pixels[i].style.backgroundColor = "#FFF";
+      customColors.innerHTML = "";
     }
   })
 
-  picker.addEventListener("change", watchColorPicker, false);
-  function watchColorPicker(event) {
-    colorPicked = picker.value;
-  }
+
 
   //Draw
   for (var i = 0; i < pixels.length; i++) {
     pixels[i].addEventListener("mouseover", function() {
       if (drawing) {
-        console.log(this);
         this.style.background = colorPicked;
       }
     });
   }
 }
-
+// Initial call
 draw();
+
 document.body.addEventListener("mousedown", function() {
   drawing = true;
 })
 document.body.addEventListener("mouseup", function() {
   drawing = false;
+})
+
+// Changes selected color
+function selectedColor() {
+  for (var i = 0; i < colors.length; i++) {
+    colors[i].addEventListener('click', function() {
+      colorPicked = window.getComputedStyle(this, null).getPropertyValue("background-color");
+    });
+  }
+}
+// Add customs colors
+addColor.addEventListener("click", function() {
+  if (colors.length < 17) {
+    var customColor = document.createElement("div");
+    customColor.className = "userColor";
+    customColor.style.background = picker.value;
+    customColors.appendChild(customColor);
+    selectedColor();
+    console.log(colors.length);
+  } else {
+    console.log("Too many colors");
+  }
 })
